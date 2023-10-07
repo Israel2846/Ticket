5bec2ee64052315b3bcb835b5a3499b276197427
Israel Colín <israaacolin@gmail.com>
2023-10-06 18:31:10 -0600

    Ordenando ascendente las subcategorías

--- models/Subcategoria.php
+++ models/Subcategoria.php
@@ -5,7 +5,7 @@
         public function get_subcategoria($cat_id){
             $conectar= parent::conexion();
             parent::set_names();
-            $sql="SELECT * FROM tm_subcategoria WHERE cat_id=? AND est=1;";
+            $sql="SELECT * FROM tm_subcategoria WHERE cat_id=? AND est=1 ORDER BY cats_nom ASC;";
             $sql=$conectar->prepare($sql);
             $sql->bindValue(1, $cat_id);
             $sql->execute();
